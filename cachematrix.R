53a4d8944012b751fa86dc3089e497215f37c75d
Santhoshi Paidiparthy <Santhoshi.Paidiparthy@gmail.com>
2015-10-23 14:20:24 -0500

    Commited cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,30 @@
 ## Assumption(As per the assignment description): The matrix supplied is always invertible. Can test using det(<your matrix>). If it returns zero, 
 ## the matrix is not invertible.
 
-## This function creates a special "matrix" object that can cache its inverse
+## This function creates a special "matrix" object that can cache its inverse.
+## makeVector creates a special "vector", which is really a list containing functions.
+
 
 makeCacheMatrix <- function(x = matrix()) {
   
   m <- NULL
+  
+  ## set the value of the matrix
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+  
+  ## get the value of the matrix
   get <- function() x
+  
+  ## set the inverse of the matrix
   setinverse <- function(inverse) m <<- inverse
+  
+  ## get the inverse of the matrix
   getinverse <- function() m
+  
+  ## Return the list containing all the above four functions.
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -27,19 +39,30 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  ## Try to get the inverse of the matrix from cache.
   m <- x$getinverse()
+  
+  ## If the inverse is already calculated..
   if(!is.null(m)) {
+    
     message("getting cached data")
+    
+  ## Return the already catched inverse of the matrix.
     return(m)
   }
+  ## If the inverse is not calculated already, get the matrix.
   data <- x$get()
+  
+  ## Use solve() function to get the inverse of the matrix.
   m <- solve(data)
+  
+  ## set the calculated inverse in the cache.
   x$setinverse(m)
+  
+  ## Return a matrix that is the inverse of 'x'.
   m
 }
 
-## How to test:
-##       
-##
+
 
